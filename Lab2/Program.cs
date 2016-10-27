9c1c4750a1301927226bdea8e88d8beece985acb
Tom Nagle <tnagle10@gmail.com>
2016-10-27 17:03:05 -0400

    Initial Version

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -8,176 +8,35 @@ namespace Lab2
 {
     class Program
     {
-        private static string getCarType ()
+
+        private static string genList(string description,string[] names)
+        {
+            Boolean valid = false;
+            int input = 0;
+            while (valid == false)
             {
-               int input = 0;
-               
-               Console.WriteLine("What kind of car do you want?");
-               Console.WriteLine("Please choose one of the following values");
-               Console.WriteLine("1. Corvette");
-               Console.WriteLine("2. Camaro");
-               Console.WriteLine("3. Charger");
-               Console.WriteLine("4. Challenger\n");
-               input = int.Parse(Console.ReadLine());
-               string carType = ""; 
-               switch (input)
-               {
-                   case 1:
-                         Console.WriteLine("\nYou chose a Corvette\n");
-                         carType = "Corvette";
-                         break;
-                   case 2:
-                         Console.WriteLine("\nYou chose a Camaro\n");
-                         carType = "Camaro";
-                         break;
-                   case 3:
-                         Console.WriteLine("\nYou chose a Charger\n");
-                         carType = "Charger";
-                         break;
-                   case 4:
-                         Console.WriteLine("\nYou chose a Challenger\n");
-                         carType = "Challenger";
-                         break;
-                   default :
-                         Console.WriteLine("You chose an invalid car\n");
-                         carType = "Invalid";
-                         break;
+
+                Console.WriteLine("Please choose one of the following values for "+description+": ");
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine(i + ": " + names[i]);
                 }
-            return carType;
+
+                input = int.Parse(Console.ReadLine());
+                int pos = Array.IndexOf(names, names[input]);
+                if (pos > -1)
+                {
+                    Console.WriteLine("You chose " + names[input]+"\n");
+                    valid = true;
+                }
+                else
+                {
+                    valid = false;
+                }
+
+
             }
-
-        private static string getCarColor ()
-        {
-            int input = 0;
-            string carColor = "";
-            Console.WriteLine("What color car do you want?");
-            Console.WriteLine("Please choose one of the following values");
-            Console.WriteLine("1. Red");
-            Console.WriteLine("2. Blue");
-            Console.WriteLine("3. Yellow");
-            Console.WriteLine("4. Orange\n");
-            input = int.Parse(Console.ReadLine());
-            switch (input)
-            {
-                case 1:
-                    Console.WriteLine("\nYou chose Red\n");
-                    carColor = "Red";
-                    break;
-                case 2:
-                    Console.WriteLine("\nYou chose Blue\n");
-                    carColor = "Blue";
-                    break;
-                case 3:
-                    Console.WriteLine("\nYou chose Yellow\n");
-                    carColor = "Yellow";
-                    break;
-                case 4:
-                    Console.WriteLine("\nYou chose Orange\n");
-                    carColor = "Orange";
-                    break;
-                default:
-                    Console.WriteLine("You chose an invalid color\n");
-                    carColor = "Invalid";
-                   break;
-            }
-            return carColor;
-        }
-
-        private static string getLocation()
-        {
-            int input = 0;
-            string location = "";
-            Console.WriteLine("Where do you want to go?");
-            Console.WriteLine("Please choose one of the following values");
-            Console.WriteLine("1. Waterworks Park");
-            Console.WriteLine("2. Duck Park");
-            
-            input = int.Parse(Console.ReadLine());
-            switch (input)
-            {
-                case 1:
-                    Console.WriteLine("\nYou chose Waterworks Park\n");
-                    location = "Waterworks Park";
-                    break;
-                case 2:
-                    Console.WriteLine("\nYou chose Duck Park\n");
-                    location = "Duck Park";
-                    break;
-                default:
-                    Console.WriteLine("You chose an invalid location\n");
-                    location = "Invalid";
-                    break;
-            }
-            return location;
-        }
-
-        private static string getWaterOptions()
-        {
-            int input = 0;
-            string location = "";
-            Console.WriteLine("Where do you want to go?");
-            Console.WriteLine("Please choose one of the following values");
-            Console.WriteLine("1. Bulldozer");
-            Console.WriteLine("2. Dinosaur");
-            Console.WriteLine("3. Swings");
-            
-
-
-            input = int.Parse(Console.ReadLine());
-            switch (input)
-            {
-                case 1:
-                    Console.WriteLine("\nYou chose the Bulldozer\n");
-                    location = "Bulldozer";
-                    break;
-                case 2:
-                    Console.WriteLine("\nYou chose the Dinosaur\n");
-                    location = "Dinosaur";
-                    break;
-                case 3:
-                    Console.WriteLine("\nYou chose the Swings\n");
-                    location = "Swings";
-                    break;
-                default:
-                    Console.WriteLine("You chose an invalid location\n");
-                    location = "Invalid";
-                    break;
-            }
-            return location;
-        }
-
-        private static string getDuckOptions()
-        {
-            int input = 0;
-            string location = "";
-            Console.WriteLine("Where do you want to go?");
-            Console.WriteLine("Please choose one of the following values");
-            Console.WriteLine("1. Duck Pond");
-            Console.WriteLine("2. Play Ground");
-            Console.WriteLine("3. Sled Hill");
-
-
-            input = int.Parse(Console.ReadLine());
-            switch (input)
-            {
-                case 1:
-                    Console.WriteLine("\nYou chose the Duck Pond\n");
-                    location = "Duck Pond";
-                    break;
-                case 2:
-                    Console.WriteLine("\nYou chose the Play Ground\n");
-                    location = "Play Ground";
-                    break;
-                case 3:
-                    Console.WriteLine("\nYou chose the Sled Hill\n");
-                    location = "Sled Hill";
-                    break;
-                default:
-                    Console.WriteLine("You chose an invalid location\n");
-                    location = "Invalid";
-                    break;
-            }
-            return location;
+            return names[input];
         }
 
         private static bool keepGoing()
@@ -212,60 +71,55 @@ namespace Lab2
 
         static void Main(string[] args)
         {
-       
-            do {
-                
+
+            do
+            {
+
                 Console.WriteLine("Do you want to go somewhere? (y or n)");
                 string input = Console.ReadLine();
                 switch (input)
                 {
-                    case "y" :
-                        string car = getCarType();
-                        string color = getCarColor();
-                        string location = getLocation();
-                        Console.WriteLine(car);
-                        Console.WriteLine(color);
-                        Console.WriteLine(location);
+                    case "y":
+                        string[] car_models = { "Corvette", "Challenger" };
+                        string[] car_colors = { "Red", "Blue", "Yellow", "Orange" };
+                        string[] locations = { "Duck Park", "Waterworks Park" };
 
-                        if (car != "Invalid" && color != "Invalid" && location != "Invalid")
-                        {
-                            switch (location)
-                            {
-                                case "Waterworks Park":
-                                    Console.WriteLine("We are driving your " + color + " " + car + " " + "to the " + location);
-                                    Console.WriteLine("Congratulations, you made it to" +location+"Where do you want to go now?");
-                                    getWaterOptions();
-                                    break;
-                                case "Duck Park":
-                                    Console.WriteLine("You made it to the Duck Park.  Where do you want to go now?");
-                                    getDuckOptions();
-                                    break;
-                                default:
-                                    break;
-                            }
-                            Console.WriteLine("We are driving your " + color + " " + car + " " + "to the " + location);
+                        string car_model = genList("car model", car_models);
+                        string car_color = genList("car color",car_colors);
+                        string location = genList("location",locations);
 
-                        }
-                        else
+
+                        switch (location)
                         {
-                            Console.WriteLine("Invalid Option Selected.  Please try again.");
-                            
+                            case "Waterworks Park":
+                                Console.WriteLine("We are driving your " + car_color + " " + car_model + " " + "to the " + location);
+                                Console.WriteLine("Congratulations, you made it to " + location + ". Where do you want to go now?");
+                                string[] waterworks_locations = { "Dinosaur", "Tractor","Sand Box"};
+                                string waterworks_location = genList("park location",waterworks_locations);
+                                break;
+                            case "Duck Park":
+                                Console.WriteLine("We are driving your " + car_color + " " + car_model + " " + "to the " + location);
+                                Console.WriteLine("Congratulations, you made it to " + location + ". Where do you want to go now?");
+                                string[] duck_locations = { "Duck Pond", "Trail", "Large Hill" };
+                                string duck_location = genList("park location",duck_locations);
+                                break;
+                                
+                            default:
+                                break;
                         }
-                        
+
                         break;
 
                     case "n":
                         Console.WriteLine("Thanks for coming");
                         return;
-                        
 
-                    default :
-                        Console.WriteLine("You chose an invalid value.\n");
-                        break;
+
                 }
-                
+
             } while (keepGoing());
-            
+
         }
     }
+
 }
